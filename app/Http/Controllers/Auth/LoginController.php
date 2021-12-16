8fe25c8d33d7c37f21d5ac47e65c55a852a3ee33
alliano <allianoanoanymous>
2021-12-16 16:16:51 +0700

    frist commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class LoginController extends Controller
 {
@@ -18,35 +20,56 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
+    
     use AuthenticatesUsers;
+    
+    // public function showLoginForm(Required $request)
+    // {
+        
+    //     return view('login');
+    // }
 
+    // public function store(Request $request){
+    //     dd($request);
+    // }
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('guest')->except('logout');
+    // }
+
+    
+
+    // public function username()
+    // {
+    //     $login = request()->input('email');
+    //     $field = filter_var($login,FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+    //     request()->merge([$login => $field]);
+    //     return $field;
+    // }
     public function showLoginForm()
     {
         return view('login');
     }
+
     public function username()
     {
         $login = request()->input('email');
         $field = filter_var($login,FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
         request()->merge([$login => $field]);
         return $field;
+
     }
 
 }
---
+++ resources/views/auth/fake.blade.php
@@ -0,0 +1,13 @@
+@@extends('navbar');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>bam</title>
+</head>
+<body>
+    <form action="" method="get"></form>
+</body>
+</html>
